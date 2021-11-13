e82e31af7009b7608401ee7cf3f8bc42ffd3ae26
bluecotton <546963830@qq.com>
2021-11-13 15:11:57 +0800

    add check cookies

--- README.md
+++ README.md
@@ -6,8 +6,8 @@
 [es](http://elasticsearch.cn/explore)  
 [GitHub deploy key](https://docs.github.com/en/developers/overview/managing-deploy-keys#deploy-keys)  
 [Bootstrap](https://v3.bootcss.com/getting-started/)  
-[GitHub OAuth](https://v3.bootcss.com/getting-started/)
-
+[GitHub OAuth](https://v3.bootcss.com/getting-started/)  
+[Spring](https://docs.spring.io/spring-boot/docs/2.0.0.RC1/reference/htmlsingle/#boot-features-embedded-database-support)
 ## 工具
 [Git](https://git-scm.com/)  
 [Visual Paradigm](https://www.visual-paradigm.com/cn/)
--- src/main/java/com/life/bluecotton/community/controller/AuthorizeController.java
+++ src/main/java/com/life/bluecotton/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.life.bluecotton.community.controller;
 
 import com.life.bluecotton.community.dto.AccessTokenDTO;
 import com.life.bluecotton.community.dto.GithubUser;
+import com.life.bluecotton.community.mapper.UserMapper;
+import com.life.bluecotton.community.model.User;
 import com.life.bluecotton.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,8 +11,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
 @Controller
-public class AuthorizeControl {
+public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;
@@ -22,9 +29,14 @@ public class AuthorizeControl {
     @Value("${github.Redirect_uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state ){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request,
+                           HttpServletResponse response) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -32,8 +44,22 @@ public class AuthorizeControl {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null) {
+            User user = new User();
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            response.addCookie(new Cookie("token", token));
+            return "redirect:/";
+
+        } else {
+            //登录失败，重新登录
+            return "redirect:/";
+        }
     }
 }
